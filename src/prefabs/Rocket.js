268746d5553a8e89ae90b70341b146fe9ec36fcd
pli60 <pli60@ucsc.edu>
2022-04-10 00:05:24 -0700

    update rocket, spaceship, and play

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -1,5 +1,3 @@
-const Phaser = require("phaser");
-
 // Rocket prefab
 class Rocket extends Phaser.GameObjects.Sprite {
         constructor(scene, x, y, texture, frame) {
@@ -7,30 +5,35 @@ class Rocket extends Phaser.GameObjects.Sprite {
                 // add object to existing scene
                 scene.add.existing(this);
                 this.isFiring = false;
-                this.movement = 2;
+                this.moveSpeed = 2;
         }
 
         update(){
-                // left/right movement
+                // left&right movement
                 if(!this.isFiring){
                         if(keyLEFT.isDown && this.x >= borderUISize + this.width){
-                                this.x -= this.movement;
+                                this.x -= this.moveSpeed;
                         } else if (keyRIGHT.isDown && this.x <= game.config.width - borderUISize - this.width){
-                                this.x += this.movement;
+                                this.x += this.moveSpeed;
                         }
                 }
                 // fire button
-                if(Phaser.Input.Keyboard.JustDown(KeyF)){
+                if(Phaser.Input.Keyboard.JustDown(keyF)){
                         this.isFiring = true;
                 }
                 // if fired, move up
                 if(this.isFiring && this.y >= borderUISize * 3 + borderPadding){
-                        this.y -= this.movement;
+                        this.y -= this.moveSpeed;
                 }
                 // reset on miss
                 if(this.y <= borderUISize *3 + borderPadding){
-                        this.isFiring = false;
-                        this.y = game.config.height - borderUISize - borderPadding;
+                        this.reset();
                 }
+        }
+
+        // reset rocket to "ground"
+        reset(){
+                this.isFiring = false;
+                this.y = game.config.height - borderUISize - borderPadding;
         }
 }
